08cd99ccff965f36b2e6b8ce98b7328d2057daca
Minato <82735346+minato-ellie@users.noreply.github.com>
2023-03-07 18:58:08 +0900

    feature(io): Write to stream

--- cvflow/io/__init__.py
+++ cvflow/io/__init__.py
@@ -1,4 +1,5 @@
 from cvflow.io.input import ImageReader, VideoDeviceReader, VideoFileReader, VideoUrlReader, VideoStreamReader
+from cvflow.io.output import VideoStreamWriter, DisplayWriter
 
 __all__ = [
     'ImageReader',
@@ -7,6 +8,9 @@ __all__ = [
     'VideoFileReader',
     'VideoUrlReader',
     'VideoStreamReader',
+
+    'VideoStreamWriter',
+    'DisplayWriter',
 
     'input',
     'output',
