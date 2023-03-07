cacb277ad6a7289add5edc21523891e4b64bd6e4
Minato <82735346+minato-ellie@users.noreply.github.com>
2023-03-07 18:55:06 +0900

    feature(io): Read from stream

---
+++ cvflow/io/__init__.py
@@ -0,0 +1,13 @@
+from cvflow.io.input import ImageReader, VideoDeviceReader, VideoFileReader, VideoUrlReader, VideoStreamReader
+
+__all__ = [
+    'ImageReader',
+
+    'VideoDeviceReader',
+    'VideoFileReader',
+    'VideoUrlReader',
+    'VideoStreamReader',
+
+    'input',
+    'output',
+]
